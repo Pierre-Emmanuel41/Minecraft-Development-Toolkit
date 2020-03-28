415bc187e018efe5c33ed05c5a869fba1f9f1947
Pierre-Emmanuel <pierre.emmanuel41@gmail.com>
2020-03-28 15:04:07 +0100

    Adding interface IExplanation

---
+++ src/main/java/fr/pederobien/minecraftdevelopmenttoolkit/interfaces/IExplanation.java
@@ -0,0 +1,16 @@
+package fr.pederobien.minecraftdevelopmenttoolkit.interfaces;
+
+public interface IExplanation {
+
+	/**
+	 * @return A string used as explanation.
+	 */
+	String get();
+
+	/**
+	 * Set the explanation text for this explanation object.
+	 * 
+	 * @param explanation The text used as explanation.
+	 */
+	void set(String explanation);
+}
