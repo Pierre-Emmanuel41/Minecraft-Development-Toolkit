3fdff67549c14f69761dfd5a957bd461ed09f2ff
Pierre-Emmanuel <pierre.emmanuel41@gmail.com>
2020-03-28 12:49:51 +0100

    Adding interface IParentEdition

---
+++ src/main/java/fr/pederobien/minecraftdevelopmenttoolkit/interfaces/IParentEdition.java
@@ -0,0 +1,16 @@
+package fr.pederobien.minecraftdevelopmenttoolkit.interfaces;
+
+public interface IParentEdition<T> {
+
+	/**
+	 * @return The object managed by this parent edition
+	 */
+	T get();
+
+	/**
+	 * Set the element managed by this parent edition.
+	 * 
+	 * @param element The new element managed by this edition.
+	 */
+	void set(T element);
+}
