9cd58548ad7ad688f0cdfe067e422183a107eec4
Pierre-Emmanuel <pierre.emmanuel41@gmail.com>
2020-03-28 13:28:53 +0100

    Adding class MinecraftToolkitException

---
+++ src/main/java/fr/pederobien/minecraftdevelopmenttoolkit/exceptions/MinecraftToolkitException.java
@@ -0,0 +1,9 @@
+package fr.pederobien.minecraftdevelopmenttoolkit.exceptions;
+
+public class MinecraftToolkitException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+
+	public MinecraftToolkitException(String message) {
+		super(message);
+	}
+}
