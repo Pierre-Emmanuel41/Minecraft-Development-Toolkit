0daeea0cf5078fd71eb7105992444f3dd9386105
Pierre-Emmanuel41 <pierre.emmanuel41@gmail.com>
2020-09-06 14:13:43 +0200

    Deleting unused classes

--- src/main/java/fr/pederobien/minecraftdevelopmenttoolkit/interfaces/messagecode/IMessageCodeEdition.java
+++
@@ -1,8 +0,0 @@
-package fr.pederobien.minecraftdevelopmenttoolkit.interfaces.messagecode;
-
-import fr.pederobien.minecraftdevelopmenttoolkit.interfaces.IGenericEdition;
-import fr.pederobien.minecraftdictionary.interfaces.IMinecraftMessageCode;
-
-public interface IMessageCodeEdition extends IGenericEdition<IMinecraftMessageCode> {
-
-}
